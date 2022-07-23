62531e7033acb28a59cc215fb1fd4bfb38ed5c87
El_Padrino <91724787+DhruvSingh-26@users.noreply.github.com>
2022-07-24 00:49:17 +0530

    Add files via upload

---
+++ youtube_video_downloader.py
@@ -0,0 +1,7 @@
+from pytube import YouTube
+from sys import argv
+link=argv[1]
+yt=YouTube(link)
+yd=yt.streams.get_highest_resolution()
+yd.download('C:/Users/ASUS/OneDrive/Desktop/youtube video')
+
